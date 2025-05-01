d845db87063785c3a47bc219a3f281a77eee6220
Gian Lajarca <gianlajarca14@gmail.com>
2025-05-01 11:25:35 +0800

    added inheritance and abstract concepts

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,10 +1,10 @@
-public class Clothing {
+public class Clothing implements Comparable<Clothing>{
     private String description;
     private double price;
     private String size = "M";
 
-    private final double MIN_PRICE = 10.0;
-    private final double MIN_TAX = .2;
+    public final static double MIN_PRICE = 10.0;
+    public final static double TAX_RATE = .2;
 
     public Clothing(String description, double price, String aSize) {
         this.description = description;
@@ -21,7 +21,7 @@ public class Clothing {
     }
 
     public double getPrice() {
-        return price + (price * MIN_TAX);
+        return price + (price * TAX_RATE);
     }
 
     public void setPrice(double price) {
@@ -37,6 +37,15 @@ public class Clothing {
         this.size = size;
     }
 
+    @Override
+    public String toString() {
+        return getDescription() + "," + getSize() + "," + getPrice();
+    }
+
+    @Override
+    public int compareTo(Clothing c) {
+        return this.description.compareTo(c.description);
+    }
 
 
 }
--- src/ShopApp.java
+++ src/ShopApp.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ShopApp {
     public static void main(String[] args) {
         double tax = 0.2;
@@ -9,6 +11,8 @@ public class ShopApp {
 
 //        c1.setName("Pinky");
 //        c1.setSize("S");
+
+        System.out.println("Min Price " + Clothing.MIN_PRICE);
 
         Clothing item1 = new Clothing("Blue Jacket", 20.9, "M");
         Clothing item2 = new Clothing("Orange T-Shirt", 10.5, "S");
@@ -28,7 +32,28 @@ public class ShopApp {
         System.out.println("Customer is " + c1.getName() + "," + c1.getSize() + "," + c1.getTotalClothingCost());
 
         for (Clothing item : c1.getItems()) {
-                System.out.println("Items " + item.getDescription());
+                //System.out.println("Items " + item.getDescription() + "," + item.getSize() + "," + item.getPrice());
+            System.out.println("Item output" + item);
             }
+
+        int average = 0;
+        int count = 0;
+
+        for (Clothing item : c1.getItems()) {
+            if (item.getSize().equals("L")) {
+                count++;
+                average += item.getPrice();
+            }
+        }
+
+        try{
+            average = (count == 0) ? 0 : average / count;
+            average = average / count;
+            System.out.println("Average price " + average + ", Count " + count);
+        } catch (ArithmeticException e) {
+            System.out.println("Don't divide by 0");
+        }
+
+        Arrays.sort(c1.getItems());
         }
     }
